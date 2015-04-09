6008f4afe2bb11af2b4d8d2e12e05c944586cbb7
Joanna <jd3012@ic.ac.uk>
2015-04-10 00:53:00 +0100

    separate into classes and can get string back to parse to Z3: TODO: fix other stuff

--- Business_Management_Processes/src/parser/code_gen.py
+++ Business_Management_Processes/src/parser/code_gen.py
@@ -1,7 +1,64 @@
 __author__ = 'joanna'
 
 from bin.z3 import *
-from Parser import *
+from p_c import p_c
+from lexer_class import *
+from ply.lex import lex
+from ply.yacc import yacc
+
+class MyParse(p_c):
+
+    tokens = lexer_class.tokens
+
+    # rules_used = []
+    # tasks = []
+    # users = []
+    # before = []
+    # seniority = []
+    # sod = []
+    # # user pairs and task pairs should be indexes???
+    #
+    # smt_sort_task = "(declare-sort Task) \n"
+    # smt_sort_user = "(declare-sort User) \n"
+    # smt_fun_before = "(declare-fun before (Task Task) Bool) \n"
+    # smt_fun_seniority = "(declare-fun seniority (User User) Bool) \n"
+    # smt_fun_sod = "(declare-fun sod (Task) User) \n"
+    # smt_fun_bod = "(declare-fun bod (Task) User) \n"
+    #
+    # smt = ""
+
+    def tokenise_input(self, input):
+        self.lexer.input(input)
+        for t in self.lexer:
+            print t
+
+# TODO: Tokenise file
+
+class code_gen(object):
+
+    def __init__(self):
+        self.lexer = lex(module=lexer_class(), optimize=1)
+        self.parser = yacc(module=MyParse(), start='prog', optimize=1)
+
+    def tokenize_string(self, code):
+        self.lexer.input(code)
+        for token in self.lexer:
+            print(token)
+
+    def parse_string(self, code, lineno=1):
+        self.lexer.lineno = lineno
+        return self.parser.parse(code, lexer=self.lexer)
+
+    def get_smt(self, code, lineno=1):
+        self.lexer.lineno = lineno
+        self.parser.parse(code, lexer=self.lexer)
+        print 'get_smt' + MyParse.smt
+        return MyParse.smt
+
+lexer = lex(module=lexer_class(), optimize=1)
+parser = yacc(module=MyParse(), start='prog', optimize=1)
+
+p = p_c()
 
 while True:
     try:
@@ -9,11 +66,19 @@ while True:
     except EOFError:
         break
     if not s: continue
-    result = parser.parse(s)
-    # Collect results to SMT solver
-    print "smt is in code gen" + smt
+    lexer.input(s)
+    for token in lexer:
+            print(token)
+    t = parser.parse(s, lexer=lexer)
+    print t
+    print "p_c is " + p_c().smt
 
-    original = smt
+
+    # Collect results to SMT solver
+    # print "smt is in code gen" + p_c.smt
+    original = MyParse.smt
+
+    print original
 
     f = z3.parse_smt2_string (original)
 
