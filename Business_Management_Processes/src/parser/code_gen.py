74c204cd9a2f36527c9e0745447af0a3a7f50d20
Joanna <jd3012@ic.ac.uk>
2015-04-14 22:42:56 +0100

    sod

--- Business_Management_Processes/src/parser/code_gen.py
+++ Business_Management_Processes/src/parser/code_gen.py
@@ -10,23 +10,6 @@ class MyParse(p_c):
 
     tokens = lexer_class.tokens
 
-    # rules_used = []
-    # tasks = []
-    # users = []
-    # before = []
-    # seniority = []
-    # sod = []
-    # # user pairs and task pairs should be indexes???
-    #
-    # smt_sort_task = "(declare-sort Task) \n"
-    # smt_sort_user = "(declare-sort User) \n"
-    # smt_fun_before = "(declare-fun before (Task Task) Bool) \n"
-    # smt_fun_seniority = "(declare-fun seniority (User User) Bool) \n"
-    # smt_fun_sod = "(declare-fun sod (Task) User) \n"
-    # smt_fun_bod = "(declare-fun bod (Task) User) \n"
-    #
-    # smt = ""
-
     def tokenise_input(self, input):
         self.lexer.input(input)
         for t in self.lexer:
@@ -58,8 +41,6 @@ class code_gen(object):
 lexer = lex(module=lexer_class(), optimize=1)
 parser = yacc(module=MyParse(), start='prog', optimize=1)
 
-p = p_c()
-
 while True:
     try:
         s = raw_input('busines_process > ')
@@ -71,37 +52,48 @@ while True:
             print(token)
     t = parser.parse(s, lexer=lexer)
     print t
-    print "p_c is " + p_c().smt
-
 
     # Collect results to SMT solver
     # print "smt is in code gen" + p_c.smt
-    original = MyParse.smt
-
+    my_parse = MyParse()
+    # original = MyParse.smt
+    original = my_parse.smt
     print original
 
-    f = z3.parse_smt2_string (original)
+#     original = """
+# (declare-sort Task)
+# (declare-const approve Task)
+# (declare-const send Task)
+# (declare-sort User)
+# (declare-const alice User)
+# (declare-const bob User)
+# (declare-fun alloc_user (Task) User)
+# (assert (not(= (alloc_user approve) (alloc_user send))))
+#  """
 
-    extra = """ """
+    f = z3.parse_smt2_string(original)
 
-    e = z3.parse_smt2_string (extra)
+    # e = z3.parse_smt2_string (extra)
 
     s = z3.Solver ()
     import sys
     if len (sys.argv) >= 2: s.push ()
     s.add (f)
-    s.add (e)
+    # s.add (e)
     print 'Result of first check', s.check ()
-    m = s.model ()
+    m = s.model()
+    print m
+
+    # Parse output into readable
     a = list ()
-    for k in m:
-        a.append (k() == m[k])
-
-    for x in a: s.add (x)
-    print 'Result of second check', s.check ()
-
-    s2 = z3.Solver ()
-    s2.add (f)
-    s2.add (e)
-    for x in a: s2.add (x)
-    print 'Independent result', s2.check ()
+    # for k in m:
+    #     a.append (k() == m[k])
+    #
+    # for x in a: s.add (x)
+    # print 'Result of second check', s.check ()
+    #
+    # s2 = z3.Solver ()
+    # s2.add (f)
+    # # s2.add (e)
+    # for x in a: s2.add (x)
+    # print 'Independent result', s2.check ()
--- Business_Management_Processes/src/parser/p_c.py
+++ Business_Management_Processes/src/parser/p_c.py
@@ -2,25 +2,27 @@ __author__ = 'joanna'
 
 class p_c(object):
     smt = ""
+
     smt_sort_task = "(declare-sort Task) \n"
+    smt_sort_task = "(declare-sort Task) \n"
+    smt_sort_user = "(declare-sort User) \n"
+    smt_fun_before = "(declare-fun before (Task Task) Bool) \n"
+    smt_fun_seniority = "(declare-fun seniority (User User) Bool) \n"
+
+    smt_fun_alloc = "(declare-fun alloc (User Task) Bool)\n"
+    smt_fun_alloc_user = "(declare-fun alloc_user (Task) User) \n"
+
+    rules_used = []
+    tasks = []
+    users = []
+
+    ended = False
 
     def __init__(self):
-        self.rules_used = []
-        self.tasks = []
-        self.users = []
         self.before = []
         self.seniority = []
         self.sod = []
         # user pairs and task pairs should be indexes???
-
-        self.smt_sort_task = "(declare-sort Task) \n"
-        self.smt_sort_user = "(declare-sort User) \n"
-        self.smt_fun_before = "(declare-fun before (Task Task) Bool) \n"
-        self.smt_fun_seniority = "(declare-fun seniority (User User) Bool) \n"
-        self.smt_fun_sod = "(declare-fun sod (Task) User) \n"
-        self.smt_fun_bod = "(declare-fun bod (Task) User) \n"
-
-        self.smt = ""
 
     # TODO: option flags
 
@@ -28,55 +30,75 @@ class p_c(object):
         '''prog : begin
                 | begin rules'''
         p_c.smt = p_c.smt.translate(None, '!@#$\'')
-        return p_c.smt
+        print p_c.tasks
+        print p_c.rules_used
+        p_c.tasks = []
+        p_c.users = []
+        p_c.rules_used = []
 
     def p_begin(self, p):
         '''begin : TASKS COLON task_node
                  | USERS COLON user_node'''
-        global smt
-        if p[1] not in self.rules_used and p[1] == 'Tasks':
-            p[0] = p[3]
-            self.rules_used.append(p[1])
+        if p[1] not in p_c.rules_used and p[1] == 'Tasks':
             p_c.smt = p_c.smt_sort_task + p_c.smt
-        elif p[1] not in self.rules_used and p[1] == 'Users':
             p[0] = p[3]
+            p_c.rules_used.append(p[1])
+        elif p[1] not in p_c.rules_used and p[1] == 'Users':
+            p_c.smt =  p_c.smt_fun_alloc_user + p_c.smt
             p_c.smt = p_c.smt_sort_user + p_c.smt
+            p_c.rules_used.append(p[1])
+            p[0] = p[3]
+            print "user"
+            print p_c.smt
         else:
-            print "p error here"
             self.p_error(p)
 
     def p_rules(self, p):
         '''rules : BEFORE COLON task_node_pair
                  | SENIORITY COLON user_node_pair
-                 | SOD COLON task_node_pair
-                 | BOD COLON task_node_pair'''
-        p[0] = p[3]
-        if p[0] == 'Before':
-            p_c.smt = self.smt_fun_before + p_c.smt
-        elif p[0] == 'Seniority':
-            p_c.smt = self.smt_fun_seniority + p_c.smt
-        elif p[0] == 'SoD':
-            p_c.smt = self.smt_fun_sod + p_c.smt
-        elif p[0] == 'BoD':
-            p_c.smt = self.smt_fun_bod + p_c.smt
+                 | BOD COLON task_node_pair
+                 | SOD COLON sod_task_node_pair'''
+        if p[1] == 'Before':
+            # p_c.smt = self.smt_fun_before + p_c.smt
+            p[0] = p[3]
+        elif p[1] == 'Seniority':
+            # p_c.smt = self.smt_fun_seniority + p_c.smt
+            p[0] = p[3]
+        elif p[1] == 'SoD':
+            # p_c.smt = self.smt_fun_sod + p_c.smt
+            p[0] = p[3]
+        elif p[1] == 'BoD':
+            # p_c.smt = self.smt_fun_bod + p_c.smt
+            p[0] = p[3]
         else:
             self.p_error(p)
 
     def p_task_pair(self, p):
         '''task_node_pair : LPAREN NODE COMMA NODE RPAREN END
                      | LPAREN NODE COMMA NODE RPAREN COMMA task_node_pair'''
-        if p[2] in self.tasks and p[4] in self.tasks:
+        if p[2] in p_c.tasks and p[4] in p_c.tasks:
+            p_c.smt += "(assert (not (= (alloc_user " + p[2] + ") (alloc_user " + p[4] + "))))\n" #+ p_c.smt
             p[0] = [p[2]] + [p[4]]
-            self.before.append(p[0])
+            # self.before.append(p[0])
+        else:
+            self.p_error(p)
+
+    def p_sod_task_pair(self, p):
+        '''sod_task_node_pair : LPAREN NODE COMMA NODE RPAREN END
+                     | LPAREN NODE COMMA NODE RPAREN COMMA task_node_pair'''
+        if p[2] in p_c.tasks and p[4] in p_c.tasks:
+            p[0] = [p[2]] + [p[4]]
+            p_c.smt = "assert (not (= (alloc " + p[2] + ") (alloc" + p[4] + ")))" + p_c.smt
+            # self.before.append(p[0])
         else:
             self.p_error(p)
 
     def p_user_pair(self, p):
         '''user_node_pair : LPAREN NODE COMMA NODE RPAREN END
                           | LPAREN NODE COMMA NODE RPAREN COMMA user_node_pair'''
-        if p[2] in self.users and p[4] in self.users:
+        if p[2] in p_c.users and p[4] in p_c.users:
             p[0] = [p[2]] + [p[4]]
-            self.seniority.append(p[0])
+            # self.seniority.append(p[0])
         else:
             self.p_error(p)
 
@@ -85,18 +107,20 @@ class p_c(object):
                 | NODE COMMA task_node
                 | NODE task_option'''
         p[0] = p[1]
-        self.tasks.append(p[0])
-        p_c.smt = "(declare-const " + p[0] + " Task)\n" + p_c.smt
+        if p[0] not in p_c.tasks:
+            p_c.tasks.append(p[0])
+            p_c.smt = "(declare-const " + p[0] + " Task)\n" + p_c.smt
 
     def p_user_node(self, p):
         '''user_node : NODE end
                 | NODE COMMA user_node
                 | NODE user_option'''
         p[0] = p[1]
-        self.users.append(p[0])
-        p_c.smt = "(declare-const " + p[0] + " User) \n" + p_c.smt
+        if p[0] not in p_c.users:
+            p_c.users.append(p[0])
+            p_c.smt = "(declare-const " + p[0] + " User) \n" + p_c.smt
 
-    def p_task_option(p):
+    def p_task_option(self,p):
         '''task_option : OPTION task_option
                   | OPTION COMMA task_node
                   | OPTION end'''
@@ -105,13 +129,20 @@ class p_c(object):
     def p_user_option(self, p):
         '''user_option : OPTION user_option
               | OPTION COMMA user_node
-              | OPTION end'''
+              | OPTION end
+              | OPTION '''
         p[0] = p[1]
 
     def p_end(self, p):
         '''end : END
                | END begin'''
         p[0] = p[1]
+        if len(p) == 2:
+            p_c.tasks = []
+            p_c.users = []
+            p_c.rules_used = []
+            p_c.smt = ""
 
-    def p_error(slelf, p):
+
+    def p_error(self, p):
         print "Syntax error in input!"
