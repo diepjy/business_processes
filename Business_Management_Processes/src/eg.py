a1a4462a72f02781e2c53bf79f4de7a9d454583e
Joanna <jd3012@ic.ac.uk>
2015-05-12 16:58:50 +0100

    clean up

--- Business_Management_Processes/src/eg.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'joanna'
-
-from bin.z3 import *
-
-p = Bool('p')
-x = Real('x')
-solve(Or(x < 5, x > 10), Or(p, x**2 == 2), Not(p))
--- Business_Management_Processes/src/function_eg.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'joanna'
-
-from bin.z3 import *
-
-x = Int('x')
-y = Int('y')
-f = Function('f', IntSort(), IntSort())
-solve(f(f(x)) == x, f(x) == y, x != y)
